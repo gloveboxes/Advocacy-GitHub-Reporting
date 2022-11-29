1047712416776181822e3f7cc5540b20c1375a61
Dave Glover <dglover@microsoft.com>
2022-11-30 10:16:13 +1100

    docs update

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\GitHub\\GitHub-Metrics-Endpoint\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\GitHub\\GitHub-Metrics-Endpoint\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\GitHub\\GitHub-Metrics-Endpoint\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\GitHub\\GitHub-Metrics-Endpoint\\src\\css\\custom.css'),
+];
